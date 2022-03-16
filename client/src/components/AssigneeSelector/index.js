3ede1795cef7384bc598c9c8f8aa9c950785a98b
Paul Vatterott <pfvatterott@gmail.com>
2022-03-16 17:09:17 -0600

    removed redundant function

--- client/src/components/AssigneeSelector/index.js
+++ client/src/components/AssigneeSelector/index.js
@@ -17,15 +17,6 @@ export default function AssigneeSelector(props) {
     })
   }
 
-  function removeAssignee() {
-    let newAssignee = {
-      task_assignee: '',
-    }
-    API.updateTask(props.id, newAssignee).then((updateTaskRes) => {
-      props.setValue(props.value + 1)
-    })
-  }
- 
   
   useEffect(() => {
     if (props.workspaceUsers.length > 0) {
@@ -35,7 +26,7 @@ export default function AssigneeSelector(props) {
         // if user is assignee of task
         if (props.workspaceUsers[i].id === props.assignees) {
           let userHTML = 
-          <a onClick={() => removeAssignee()}>
+          <a onClick={() => addAssignee('')}>
             <img src={props.workspaceUsers[i].img} className="circle user_image_selected_assignee left"></img>
             <div className="dropdown_selection_text">{props.workspaceUsers[i].first_name} {props.workspaceUsers[i].last_name}</div>
           </a>
